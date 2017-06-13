43da411645e3b7e8aa79926ba5fc03f8abfd35fb
ushanarayanan <usha.narayanan@gmail.com>
2017-06-13 16:29:42 -0700

    Initial commit

---
+++ app/src/main/java/com/apps/tejasnarayanan/monopolybank/JoinGameActivity.java
@@ -0,0 +1,13 @@
+package com.apps.tejasnarayanan.monopolybank;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JoinGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join_game);
+    }
+}
